09c1e6f79ebe8ae2b86fa567143340c40bc81780
Emre Berber <yusuf-emre1999@hotmail.com>
2019-12-20 12:35:18 +0100

    Added some attributes for the player class

--- src/game/player/Player.java
+++ src/game/player/Player.java
@@ -1,5 +1,21 @@
 package game.player;
 
+import java.io.File;
+
 public class Player {
 
+	 public int id ;
+	 public int maxHP ;
+	 public int currentHP;
+	 
+	 int[] Gear = new int[3]; // int[0] = Head, int[1] = Body, int[2] = Legs, int[3] = Hand
+	 int[] Quickslots = new int[3]; 
+	 int[] Inventory = new int[8];
+	 
+	 double x;
+	 double y;
+	 
+	File playerFile;
+	 
+	 
 }
