53f9016202ce7966422492a73e13ee6c436b4b4e
Emre Berber <yusuf-emre1999@hotmail.com>
2019-12-20 12:34:56 +0100

    docBuilder and fileBuilder added in DataManager

--- src/io/data/DataManager.java
+++ src/io/data/DataManager.java
@@ -2,6 +2,18 @@ package io.data;
 
 import java.io.File;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+
 public class DataManager {
 	
 	public static File map;
@@ -10,7 +22,38 @@ public class DataManager {
 	public static File savegame;
 
 	public static void setupDirs() {
-
+		     
+		     
 	}
-
+	
+	public static Document docBuilder() {
+		
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	    DocumentBuilder dBuilder = null;
+		try {
+			dBuilder = dbFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Document doc = dBuilder.newDocument();
+		return doc;
+	}
+	
+	public static void fileBuilder(Document doc, String pathname) {
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		try {
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+	        StreamResult result = new StreamResult(new File(pathname));
+	        transformer.transform(source, result);
+		} catch (TransformerConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+ 	
 }
