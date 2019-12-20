f0c9610138ff4df6cc7699b326e455066a7387f8
Emre Berber <yusuf-emre1999@hotmail.com>
2019-12-20 12:37:21 +0100

    Added XMLnpc

---
+++ src/io/data/text/XMLnpc.java
@@ -0,0 +1,21 @@
+package io.data.text;
+
+import java.io.File;
+
+public class XMLnpc {
+
+	File npcFile;
+
+	public XMLnpc(File npcFile) {
+		this.npcFile = npcFile;
+	}
+
+	public void read() {
+
+	}
+
+	public void write() {
+
+	}
+
+}
