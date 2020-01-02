680d62c89aeaf09541cf97554ec10023f3566345
Jan B. <keksgesicht23@gmail.com>
2020-01-02 22:18:39 +0100

    test images and image loading

--- src/io/data/DataManager.java
+++ src/io/data/DataManager.java
@@ -1,8 +1,13 @@
 package io.data;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
-public class DataManager {
+import javax.imageio.ImageIO;
+
+public abstract class DataManager {
 	
 	public static File map;
 	public static File cfg;
@@ -11,6 +16,21 @@ public class DataManager {
 
 	public static void setupDirs() {
 
+	}
+
+	/**
+	 * Lädt ein Bild aus den Resourcen
+	 * 
+	 * @param name der Name der Datei
+	 * @return das Bild als {@link BufferedImage}-Objekt
+	 * @throws IOException Eine IOException wird geworfen, falls das Bild nicht
+	 *                     gefunden wurde oder andere Probleme beim Laden auftreten
+	 */
+	public static BufferedImage loadImage(String name) throws IOException {
+		URL res = DataManager.class.getClassLoader().getResource(name);
+		if (res == null)
+			throw new IOException("Resource not found: " + name);
+		return ImageIO.read(res);
 	}
 
 }
