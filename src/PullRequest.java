32e0bb0f2f0796a9d204c02b71406f9649203f98
Jan B. <keksgesicht23@gmail.com>
2020-03-04 14:45:28 +0100

    preparing pull request testing

---
+++ src/PullRequest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jan Braun
+ *
+ */
+public class PullRequest {
+
+	/**
+	 * 
+	 */
+	public PullRequest() {
+		// TODO Auto-generated constructor stub
+		int i = 0;
+		System.out.print(i);
+	}
+
+}
