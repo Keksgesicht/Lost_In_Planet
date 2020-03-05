93e15fdb13bf60941c98ddcf018ba912fa13a94d
Jan B. <keksgesicht23@gmail.com>
2020-03-05 15:36:20 +0100

    block has position

--- src/game/resources/GameVisibleItem.java
+++ src/game/resources/GameVisibleItem.java
@@ -1,11 +1,14 @@
 package game.resources;
 
+import java.awt.Point;
+
 import javafx.scene.image.Image;
 
 public abstract class GameVisibleItem {
 
 	private final String name;
 	private final Image image;
+	private Point position;
 
 	/**
 	 * @param image the pictures that will be rendered in the GUI
@@ -28,6 +31,22 @@ public abstract class GameVisibleItem {
 	 */
 	public Image image() {
 		return image;
+	}
+
+	public int posX() {
+		return position.x;
+	}
+
+	public int posY() {
+		return position.y;
+	}
+
+	public void posX(int x) {
+		position.x = x;
+	}
+
+	public void posY(int y) {
+		position.y = y;
 	}
 
 }
